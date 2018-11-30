42d8fac69fb7d54a6f864de6ff2cb3ae647a4a02
Issa <abdu2997@edu.easj.dk>
2018-11-30 18:04:29 +0100

    HealthReciever

---
+++ BerthaUdpReciever/Health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerthaUdpReciever
+{
+    class Health
+    {
+
+        public int BloodPressure { get; set; }
+        public int HeartBeat { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateTime { get; set; }
+        public Health( int bloodPressure, int age, int weight, int userId, DateTime dateTime)
+        {
+            BloodPressure = bloodPressure;
+            Age = age;
+            Weight = weight;
+            UserId = userId;
+            DateTime = dateTime;
+        }
+
+        public Health()
+        {
+        }
+    }
+}
--- BerthaUdpReciever/Program.cs
+++ BerthaUdpReciever/Program.cs
@@ -13,15 +13,180 @@ namespace BerthaUdpReciever
 {
     class Program
     {
+
         static readonly Temp te = new Temp();
+        static readonly Health he=new Health();
         static readonly HttpClient Client = new HttpClient();
         private static string TempUri = "https://birthawebservice20181031094923.azurewebsites.net/api/environment";
+        private static string HealthUri = "https://birthawebservice20181031094923.azurewebsites.net/api/health";
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter -T- for temp Data, -H- for health data or stop \r\n");
+            string option = Console.ReadLine();
 
-      
-   
+            try
+            {
+                while (!string.Equals(option, "stop", StringComparison.Ordinal))
+                {
+                    if (option != null)
+                    {
+                        switch (option.ToUpper())
+                        {
+                            case "T":
+                                Temp().GetAwaiter().GetResult();
+                                break;
+                            case "H":
+                                Health().GetAwaiter().GetResult();
+
+                                break;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        static async Task AddToPost(Temp Temp)
+        {
+            var jsonString = JsonConvert.SerializeObject(Temp);
+            //Console.WriteLine("JSON:: " + jsonString);
+            StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                HttpResponseMessage response = await Client.PostAsync(TempUri, content);
+                //Console.WriteLine(response);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+
+        static async Task AddToPostHealth(Health health)
+        {
+            var jsonString = JsonConvert.SerializeObject(health);
+            //Console.WriteLine("JSON:: " + jsonString);
+            StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                HttpResponseMessage response = await Client.PostAsync(HealthUri, content);
+                //Console.WriteLine(response);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+
+
+       
+
+        static async Task Health()
+        {
+            int UserId = 0;
+            int BloodPressure = 0;
+            int HeartBeat = 0;
+            int Age = 0;
+            int Weight = 0;
+
+            DateTime date = DateTime.Now;
+
+            UdpClient udpReceiver = new UdpClient(7000);
+
+            // This IPEndPoint will allow you to read datagrams sent from any ip-source on port 9000
+
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 9000);
+
+            // receivingUdpClient.Connect(RemoteIpEndPoint); what is this used for ??
+
+            // Blocks until a message returns on this socket from a remote host.
+            Console.WriteLine("Health Receiver is blocked");
+
+            try
+            {
+                while (true)
+                {
+                    Byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    if (receivedData.Equals("STOP.Secret")) throw new Exception("Receiver stopped");
+                    Console.WriteLine("Sender: " + receivedData.ToString());
+
+                    string[] textLines = receivedData.Split('\n');
+
+                    for (int index = 0; index < textLines.Length; index++)
+                        Console.Write(textLines[index]);
+                    //0 Dat
+                    //1 user id
+                    //2 blood
+                    //3 heart
+                    //4 age  
+                    //5we
+                    //6
+                    string[] list0 = textLines[0].Split();
+                    string text0 = list0[1];
+                    string[] list1 = textLines[1].Split();
+                    string text1 = list1[1];
+                    string[] list2 = textLines[2].Split();
+                    string text2 = list2[1];
+                    string[] list3 = textLines[3].Split();
+                    string text3 = list3[1];
+                    string[] list4 = textLines[4].Split();
+                    string text4 = list4[1];
+                    string[] list5 = textLines[5].Split();
+                    string text5 = list5[1];
+
+                    UserId = Int32.Parse(text1);
+                    BloodPressure = Int32.Parse(text2);
+                    HeartBeat = Int32.Parse(text3);
+                    Age = Int32.Parse(text4);
+                    Weight = Int32.Parse(text5);
+                    
+
+
+                    he.UserId = UserId;
+                    he.BloodPressure = BloodPressure;
+                    he.HeartBeat = HeartBeat;
+                    he.Age = Age;
+                    he.Weight = Weight;
+                    he.DateTime = date;
+
+
+
+                   AddToPostHealth(he).GetAwaiter().GetResult();
+
+
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine();
+        }
+
+
+
+        static async Task Temp()
+        {
             int UserId = 0;
             decimal Humidity = 0;
             decimal Temperature = 0;
@@ -47,11 +212,8 @@ namespace BerthaUdpReciever
                 while (true)
                 {
                     Byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
-
-
                     string receivedData = Encoding.ASCII.GetString(receiveBytes);
                     if (receivedData.Equals("STOP.Secret")) throw new Exception("Receiver stopped");
-
                     Console.WriteLine("Sender: " + receivedData.ToString());
 
                     string[] textLines = receivedData.Split('\n');
@@ -59,10 +221,6 @@ namespace BerthaUdpReciever
                     for (int index = 0; index < textLines.Length; index++)
                         Console.Write(textLines[index]);
 
-                    //1 id
-                    //2 hum
-                    //3 temp
-                    //4 loc
                     string[] list0 = textLines[0].Split();
                     string text0 = list0[1];
                     string[] list1 = textLines[1].Split();
@@ -84,13 +242,12 @@ namespace BerthaUdpReciever
                     te.Location = Location;
                     te.DateTime = date;
 
+
                     AddToPost(te).GetAwaiter().GetResult();
 
 
 
                     Thread.Sleep(1000);
-
-                    //number++;
                 }
             }
             catch (Exception e)
@@ -101,36 +258,5 @@ namespace BerthaUdpReciever
             Console.WriteLine();
         }
 
-        static async Task AddToPost(Temp Temp)
-        {
-            var jsonString = JsonConvert.SerializeObject(Temp);
-            Console.WriteLine("JSON " + jsonString);
-            StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-            try
-            {
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-                HttpResponseMessage response = await Client.PostAsync(TempUri, content);
-                Console.WriteLine(response);
-
-                response.EnsureSuccessStatusCode();
-
-
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                throw;
-            }
-
-
-
-
-
-        
-
-
-        }
     }
 }
